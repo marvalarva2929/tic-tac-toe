86389075f7285c6dd5628e432f15c56e2372d399
marvalarva2929 <56195553+marvalarva2929@users.noreply.github.com>
2020-10-14 19:53:40 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(7);
+  line(200,50,200,550);
+  line(400,50,400,550);
+  line(50,200,550,200);
+  line(50,400,550,400);
+}
